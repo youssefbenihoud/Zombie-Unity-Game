250b0be7f52ca9e49808021d05b6abe21c71d1d2
Youssef Benihoud <youss.benih@gmail.com>
2019-02-01 00:27:44 +0000

    FinishTheGame

--- ZombieLynda/Assets/Scripts/GameController.cs
+++ ZombieLynda/Assets/Scripts/GameController.cs
@@ -1,25 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
 
 public class GameController : MonoBehaviour
 {
 
 	public GameObject playerPrefab;
+	public Text continueText;
+	public Text scoreText;
 
+	private float timeElapsed = 0f;
+	private float bestTime = 0f;
+	private float blinkTime = 0f;
+	private bool blink;
 	private bool gameStarted;
-
 	private TimeManager timeManager;
 	private GameObject player;
 	private GameObject floor;
 	private Spawner spawner;
+	private bool beatBestTime;
 
 	void Awake ()
 	{
 		floor = GameObject.Find ("Forground");
 		spawner = GameObject.Find ("Spawner").GetComponent<Spawner> ();
 		timeManager = GetComponent<TimeManager> ();
-
 	}
 
 	// Use this for initialization
@@ -27,56 +34,92 @@ public class GameController : MonoBehaviour
 	{
 
 		var floorHeight = floor.transform.localScale.y;
+
 		var pos = floor.transform.position;
-		pos.x = 0f;
+		pos.x = 0;
 		pos.y = -((Screen.height / PixelPerfectCamera.pixelsToUnits) / 2) + (floorHeight / 2);
 		floor.transform.position = pos;
 
 		spawner.active = false;
 
-		//Time.timeScale = 0f;
+		Time.timeScale = 0;
 
-		//ResetGame ();
+		continueText.text = "PRESS ANY BUTTON TO START";
+
+		bestTime = PlayerPrefs.GetFloat ("BestTime");
 	}
-	
+
 	// Update is called once per frame
 	void Update ()
 	{
-		if (!gameStarted) {
+		if (!gameStarted && Time.timeScale == 0) {
+
 			if (Input.anyKeyDown) {
+
 				timeManager.ManipulateTime (1, 1f);
 				ResetGame ();
 			}
 		}
-	}
 
+		if (!gameStarted) {
+			blinkTime++;
+
+			if (blinkTime % 40 == 0) {
+				blink = !blink;
+			}
+
+			continueText.canvasRenderer.SetAlpha (blink ? 0 : 1);
+
+			var textColor = beatBestTime ? "#FF0" : "#FFF";
+
+			scoreText.text = "TIME: " + FormatTime (timeElapsed) + "\n<color=" + textColor + ">BEST: " + FormatTime (bestTime) + "</color>";
+		} else {
+			timeElapsed += Time.deltaTime;
+			scoreText.text = "TIME: " + FormatTime (timeElapsed);
+		}
+	}
 
 	void OnPlayerKilled ()
 	{
 		spawner.active = false;
 
 		var playerDestroyScript = player.GetComponent<DestroyOffScreen> ();
-
 		playerDestroyScript.DestroyCallBack -= OnPlayerKilled;
 
 		player.GetComponent<Rigidbody2D> ().velocity = Vector2.zero;
 		timeManager.ManipulateTime (0, 5.5f);
 		gameStarted = false;
+
+		continueText.text = "PRESS ANY BUTTON TO RESTART";
+
+		if (timeElapsed > bestTime) {
+			bestTime = timeElapsed;
+			PlayerPrefs.SetFloat ("BestTime", bestTime);
+			beatBestTime = true;
+		}
 	}
 
-	public void ResetGame ()
+	void ResetGame ()
 	{
-
 		spawner.active = true;
 
-		player = GameObjectUtil.Instantiate (playerPrefab, new Vector3 (0, (Screen.height / PixelPerfectCamera.pixelsToUnits) / 2 + 50, 0));
+		player = GameObjectUtil.Instantiate (playerPrefab, new Vector3 (0, (Screen.height / PixelPerfectCamera.pixelsToUnits) / 2 + 100, 0));
 
 		var playerDestroyScript = player.GetComponent<DestroyOffScreen> ();
-
 		playerDestroyScript.DestroyCallBack += OnPlayerKilled;
 
 		gameStarted = true;
 
+		continueText.canvasRenderer.SetAlpha (0);
 
+		timeElapsed = 0;
+		beatBestTime = false;
+	}
+
+	string FormatTime (float value)
+	{
+		TimeSpan t = TimeSpan.FromSeconds (value);
+
+		return string.Format ("{0:D2}:{1:D2}", t.Minutes, t.Seconds);
 	}
 }
